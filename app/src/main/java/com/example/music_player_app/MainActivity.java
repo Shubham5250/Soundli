07ac538f8fc8b65560ff3ffd8188e1341d82a3ba
Shubham5250 <shubhampaw312@gmail.com>
2022-12-18 00:50:03 +0530

    added mainactivity-layout

--- app/src/main/java/com/example/music_player_app/MainActivity.java
+++ app/src/main/java/com/example/music_player_app/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.music_player_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button play;
+    private Button prev;
+    private Button next;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.play);
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.insane);
+        mp.start();
+        }
     }
-}
