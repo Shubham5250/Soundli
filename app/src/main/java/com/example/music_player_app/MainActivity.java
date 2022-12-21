3a83aef2c87a54c3fa38f07c5be214317f41d701
Shubham5250 <shubhampaw312@gmail.com>
2022-12-21 14:11:51 +0530

    seekBar & songs addedk

--- app/src/main/java/com/example/music_player_app/MainActivity.java
+++ app/src/main/java/com/example/music_player_app/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.music_player_app;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.media.AudioManager;
+
+
+
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -12,45 +11,26 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity<Textview> extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
-    private Textview songTitle;
-    private MediaPlayer mediaPlayer;
-    private Runnable runnable;
-    private AudioManager audioManager;
+    MediaPlayer mediaPlayer;
     ImageView play;
     ImageView skipNext;
+    TextView textView2;
     ImageView skipPrev;
 
-    int currentIndex =0;
 
-    public MainActivity(Textview songTitle) {
-        this.songTitle = songTitle;
-        if(currentIndex == 0){
-            (new TextView((Context) songTitle)).setText("Yugat Mandli.mp3");
-        }
-        if(currentIndex == 1){
-            (new TextView((Context) songTitle)).setText("Shivba Raja shivba raja.mp3");
-        }if(currentIndex == 2){
-            (new TextView((Context) songTitle)).setText("Shwasat raja dhyasat raja.mp3");
-        }
-        if(currentIndex == 3){
-            (new TextView((Context) songTitle)).setText("Majhya Raja Ra.mp3");
-        }
-        if(currentIndex == 4){
-            (new TextView((Context) songTitle)).setText("Sawari Bhavani Chauka madhi.mp3");
-        }
-    }
+    int currentIndex = 0;
+
 
     //Arraylist to store our songs
-
-
-
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,148 +40,172 @@ public class MainActivity<Textview> extends AppCompatActivity {
         play = findViewById(R.id.play);
         seekBar = findViewById(R.id.seekBar);
         skipNext = findViewById(R.id.skipNext);
-        final Textview songTitle = (Textview) findViewById(R.id.songTitle);
+        skipPrev = findViewById(R.id.skipPrev);
+        textView2 = findViewById(R.id.textView2);
 
-        ArrayList<Integer> songs = new ArrayList<>();
-        songs.add(0,R.raw.yugatMandali);
-        songs.add(1,R.raw.shivbaRaja);
-        songs.add(2,R.raw.shwasatRaja);
-        songs.add(3,R.raw.majhyaRajaRa);
-        songs.add(4,R.raw.sawari);
+        final ArrayList<Integer> songs = new ArrayList<>();
+        songs.add(0, R.raw.yugat);
+        songs.add(1, R.raw.shivba);
+        songs.add(2, R.raw.shwasat);
+        //songs.add(3, R.raw.insane);
+        //songs.add(4, R.raw.excuse);
 
 
-        //initializing the mediaplayer
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),songs.get(currentIndex));
-        mediaPlayer.start();
+        //initializing the mediaPlayer
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
 
         play.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                if(!mediaPlayer.isPlaying()){
-                    mediaPlayer.start();
-                    play.setImageResource(R.drawable.ic_baseline_pause_circle_24);
-                }else{
+                seekBar.setMax(mediaPlayer.getDuration());
+
+
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
                     play.setImageResource(R.drawable.ic_baseline_play_circle_24);
+                } else {
+                    mediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_baseline_pause_circle_24);
                 }
+
                 songNames();
+            }
+
+            private void songNames() {
+                if (currentIndex == 0) {
+                    textView2.setText("Yugat Mandali");
+                }
+                if (currentIndex == 1) {
+                    textView2.setText("Shivba Raja");
+                }
+                if (currentIndex == 2) {
+                    textView2.setText("Shwasat raja ra dhyasat raja");
+                }
+
             }
         });
 
         skipNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer !=null){
-                    play.setImageResource(R.drawable.ic_baseline_play_circle_24);
+                if (mediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_baseline_pause_circle_24);
+
                 }
 
-                if(currentIndex < songs.size()-1 ){
+                if (currentIndex < songs.size() - 1) {
                     currentIndex++;
-                }
-
-                else{
+                } else {
                     currentIndex = 0;
                 }
 
-                if(mediaPlayer.isPlaying()){
+
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                 }
 
-                mediaPlayer = MediaPlayer.create(getApplicationContext(),songs.get(currentIndex));
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
                 mediaPlayer.start();
                 songNames();
+            }
+
+            private void songNames() {
+                if (currentIndex == 0) {
+                    textView2.setText("Yugat Mandali");
+                }
+                if (currentIndex == 1) {
+                    textView2.setText("Shivba Raja");
+                }
+                if (currentIndex == 2) {
+                    textView2.setText("Shwasat raja ra dhyasat raja");
+                }
+
             }
         });
 
         skipPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer !=null){
+                if (mediaPlayer != null) {
                     play.setImageResource(R.drawable.ic_baseline_play_circle_24);
                 }
 
-                if(currentIndex >0 ){
+                if (currentIndex > 0) {
                     currentIndex--;
+                } else {
+                    currentIndex = songs.size() - 1;
                 }
 
-                else{
-                    currentIndex = songs.size()-1;
+
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
                 }
 
-                if(mediaPlayer.isPlaying()){
-                    mediaPlayer.start();
-                }
-
-                mediaPlayer = MediaPlayer.create(getApplicationContext(),songs.get(currentIndex));
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
                 mediaPlayer.start();
                 songNames();
+            }
+
+            private void songNames() {
+                if (currentIndex == 0) {
+                    textView2.setText("Yugat Mandali");
+                }
+                if (currentIndex == 1) {
+                    textView2.setText("Shivba Raja");
+                }
+                if (currentIndex == 2) {
+                    textView2.setText("Shwasat raja ra dhyasat raja");
+                }
+
             }
 
         });
 
 
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                mediaPlayer.start();
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
 
-
-//        mediaPlayer = new MediaPlayer();
-//        try {
-//            mediaPlayer.setDataSource("http://socialdance.stanford.edu/music/Moskwa.mp3");
-//            //mediaPlayer.prepare();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-            seekBar.setMax(mediaPlayer.getDuration());
-            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                   if(fromUser){
-                        mediaPlayer.seekTo(progress);}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                    seekBar.setProgress(progress);
                 }
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-
-
-//        play.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mediaPlayer.start();
-//
-//            }
-//        });
-//        prev.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mediaPlayer.pause();
-//            }
-//        });
-        }
-
-        private void songNames(){
-            if(currentIndex == 0){
-                (new TextView((Context) songTitle)).setText("Yugat Mandli.mp3");
             }
-            if(currentIndex == 1){
-                (new TextView((Context) songTitle)).setText("Shivba Raja shivba raja.mp3");
-            }if(currentIndex == 2){
-                (new TextView((Context) songTitle)).setText("Shwasat raja dhyasat raja.mp3");
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
             }
-            if(currentIndex == 3){
-                (new TextView((Context) songTitle)).setText("Majhya Raja Ra.mp3");
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
-            if(currentIndex == 4){
-                (new TextView((Context) songTitle)).setText("Sawari Bhavani Chauka madhi.mp3");
-            }
-        }
+        });
+
 
     }
+}
+
+
+
+
+
